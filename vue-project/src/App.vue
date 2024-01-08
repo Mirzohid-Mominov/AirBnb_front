92a7f2f7a6bdc73be941670fc5f5d3860ffed9ba
Mirzohid-Mominov <momirzohid4@gmail.com>
2024-01-08 15:04:45 +0500

    Updated Some services

---
+++ vue-project/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+ <!-- <main-logo/> -->
+  <home-view/>
+</template>
+<script setup lang="ts">
+import MainLogo from "@/components/icons/MainLogo.vue";
+import HomeView from "@/common/Components/HomeView.vue";
+</script>
